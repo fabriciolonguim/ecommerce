951d24d49345b63fac4d26398295b1244189c694
fabricolonguim <fabricio@fabriciounix.com.br>
2018-07-04 15:41:46 -0300

    criando o pageAdmin e o admintpl

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
-
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
@@ -20,6 +20,14 @@ $app->get('/', function() {
 	echo json_encode($results);
     */
     $page = new Page();
+
+    $page->setTpl("index");
+	
+});
+
+$app->get('/admin', function() {
+    
+    $page = new PageAdmin();
 
     $page->setTpl("index");
 	
